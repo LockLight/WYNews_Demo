2f15eb9ab8774a1dc863ef43856d74bcd34e0076
LockLight <Locklightz@gmail.com>
2017-03-18 14:12:00 +0800

    Initial Commit

---
+++ 网易新闻Demo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  网易新闻Demo
+//
+//  Created by locklight on 17/3/18.
+//  Copyright © 2017年 LockLight. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
---
+++ 网易新闻Demo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  网易新闻Demo
+//
+//  Created by locklight on 17/3/18.
+//  Copyright © 2017年 LockLight. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
